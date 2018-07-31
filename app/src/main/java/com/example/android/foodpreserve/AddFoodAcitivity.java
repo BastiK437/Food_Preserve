ae9823484f1c1a9fb3b19c4da2411204926d6e17
Bastian Krammer <Bastian.Krammer@haw-hamburg.de>
2018-07-31 16:08:23 +0200

    Datum eintragen rumspielen mit Dropdown menue

--- app/src/main/java/com/example/android/foodpreserve/AddFoodAcitivity.java
+++ app/src/main/java/com/example/android/foodpreserve/AddFoodAcitivity.java
@@ -2,7 +2,8 @@ package com.example.android.foodpreserve;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.CalendarView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class AddFoodAcitivity extends AppCompatActivity {
 
@@ -10,15 +11,15 @@ public class AddFoodAcitivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_food_acitivity);
+
+        //get the spinner from the xml.
+        Spinner dropdown = findViewById(R.id.mhd_day);
+        String[] items = new String[]{"1", "2", "3", "4", "5"};
+        //create an adapter to describe how the items are displayed, adapters are used in several places in android.
+        //There are multiple variations of this, but this is the basic variant.
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
+        //set the spinners adapter to the previously created one.
+        dropdown.setAdapter(adapter);
     }
 
-    CalendarView.setOnDateChangeListener(new OnDateChangeListener() {
-
-        @Override
-        public void onSelectedDayChange(CalendarView view, int year, int month,
-        int dayOfMonth) {
-            int d = dayOfMonth;
-            curDate =String.valueOf(d);
-        }
-    });
 }
