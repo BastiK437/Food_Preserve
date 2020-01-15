b07e4f4c28e397dea28e0c55faa3349b2a3c196e
Bastian Krammer <bastiankrammer@googlemail.com>
2020-01-15 21:23:05 +0100

    switched to flutter

--- app/src/main/java/com/example/android/foodpreserve/NumberReadException.java
+++
@@ -1,8 +0,0 @@
-package com.example.android.foodpreserve;
-
-public class NumberReadException extends Exception {
-
-    public NumberReadException(String errorMessage){
-        super(errorMessage);
-    }
-}
---
+++ lib/foodData.g.dart
@@ -0,0 +1,19 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'foodData.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+FoodData _$FoodDataFromJson(Map<String, dynamic> json) {
+  return FoodData(
+    json['name'] as String,
+    json['email'] as String,
+  );
+}
+
+Map<String, dynamic> _$FoodDataToJson(FoodData instance) => <String, dynamic>{
+      'name': instance.name,
+      'email': instance.email,
+    };
