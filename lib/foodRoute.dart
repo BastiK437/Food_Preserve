31fc1875739151334d29cb28cbb126ed00fdd7bd
Bastian Krammer <bastiankrammer@googlemail.com>
2020-01-15 21:27:04 +0100

    wip

---
+++ lib/foodRoute.dart
@@ -0,0 +1,77 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+import 'foodWidget.dart';
+
+// TODO: Check if we need to import anything
+
+// TODO: Define any constants
+const _noe = 8;
+
+/// Category Route (screen).
+///
+/// This is the 'home' screen of the Unit Converter. It shows a header and
+/// a list of [Categories].
+///
+/// While it is named CategoryRoute, a more apt name would be CategoryScreen,
+/// because it is responsible for the UI at the route's destination.
+class foodRoute extends StatelessWidget {
+  const foodRoute();
+
+  static const _categoryNames = <String>[
+    'Length',
+    'Area',
+    'Volume',
+    'Mass',
+    'Time',
+    'Digital Storage',
+    'Energy',
+    'Currency',
+  ];
+
+  static const _baseColors = <Color>[
+    Colors.teal,
+    Colors.orange,
+    Colors.pinkAccent,
+    Colors.blueAccent,
+    Colors.yellow,
+    Colors.greenAccent,
+    Colors.purpleAccent,
+    Colors.red,
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: Create a list of the eight Categories, using the names and colors
+    // from above. Use a placeholder icon, such as `Icons.cake` for each
+    // Category. We'll add custom icons later.
+    final categorys = List<FoodWidget>.generate(_noe, (i) => FoodWidget(_categoryNames[i], null, _baseColors[i]));
+
+    // TODO: Create a list view of the Categories
+    final listView = Container(
+      child: ListView(
+        children: categorys,
+      ),
+    );
+
+    // TODO: Create an App Bar
+    final appBar = AppBar(
+      elevation: 0.0,
+      centerTitle: true,
+      title: Text(
+        'Unit Converter',
+        style: TextStyle(
+          fontSize: 30.0,
+        ),
+      ),
+
+    );
+
+    return Scaffold(
+      appBar: appBar,
+      body: listView,
+    );
+  }
+}
